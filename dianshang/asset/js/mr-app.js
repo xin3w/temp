34e47c84fab472760680c2d4e91eab561f8a5706
xin3w <xin3w@126.com>
2017-09-20 17:59:04 +0800

    修改

--- dianshang/asset/js/mr-app.js
+++ dianshang/asset/js/mr-app.js
@@ -201,4 +201,21 @@ addEventListener('load', function() {
 	orientationChange();
 	window.onorientationchange = orientationChange;
 });
+//禁止下拉弹性显示
+window.onload = function() {
+	var lastY; //最后一次y坐标点
+	$(document.body).on('touchstart', function(event) {
+		lastY = event.originalEvent.changedTouches[0].clientY; //点击屏幕时记录最后一次Y度坐标。
+	});
+	$(document.body).on('touchmove', function(event) {
+		var y = event.originalEvent.changedTouches[0].clientY;
+		var st = $(this).scrollTop(); //滚动条高度  
+		if(y >= lastY && st <= 10) { //如果滚动条高度小于0，可以理解为到顶了，且是下拉情况下，阻止touchmove事件。
+			lastY = y;
+			event.preventDefault();
+		}
+		lastY = y;
+
+	});
+}
 //# sourceMappingURL=../maps/mr-app.js.map
